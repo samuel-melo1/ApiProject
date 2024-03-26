ed759792304689e00a3b161c957f5d1d65d1adfe
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-03-25 22:15:45 -0300

    Order service, order resource and order service implementation

---
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -0,0 +1,35 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.OrderService;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderResource {
+
+    @Autowired
+    private OrderService service;
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll(){
+        List<Order> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> findUserById(@PathVariable Long id){
+        Order obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+}
