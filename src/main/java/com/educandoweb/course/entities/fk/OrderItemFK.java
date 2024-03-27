93b70a0b7b35b0c5a98987c140266e092e00e512
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-03-26 23:02:23 -0300

    created and implemented order and item relationship

---
+++ src/main/java/com/educandoweb/course/entities/fk/OrderItemFK.java
@@ -0,0 +1,46 @@
+package com.educandoweb.course.entities.fk;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemFK implements Serializable {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+    public Order getOrder() {
+        return order;
+    }
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemFK orderItem = (OrderItemFK) o;
+        return Objects.equals(getOrder(), orderItem.getOrder()) && Objects.equals(getProduct(), orderItem.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
